0fb2e91c4da44f94093e79831b2f8e00fc34d147
VanNHPH09573 <vannhph09573@gmail.com>
2021-07-13 08:34:06 +0700

    update

--- hanoioffice_user/src/component/place/leVanLuong/index.jsx
+++ hanoioffice_user/src/component/place/leVanLuong/index.jsx
@@ -1,9 +1,80 @@
 import React from 'react';
 
+import {NavLink} from 
+  'react-router-dom';
+
 const LeVanLuong = () => {
   return (
     <>
-      <h1>{ 'Viết code cho màn hình "địa điểm Lê Văn Lương" tại đây' }</h1>
+      <div className="bg-black py-36">
+        <h1 className="text-white text-center text-3xl">Hà Nội Office - Lê Văn Lương</h1>
+      </div>
+      <div className="pt-6 border-b border-gray-300"></div>
+      <div className="pt-4">
+        <div className="grid grid-cols-5 gap-6 mx-96">
+          <div className="col-span-4">
+            <div className="grid grid-cols-4 gap-4">
+              <div className="border-l border-gray-400 box-content">
+                <div className="pl-4">
+                  <div className="grid grid-cols-5">
+                    <div className="col-span-1">
+                      <img src="https://hanoioffice.vn/wp-content/plugins/mkdf-real-estate/assets/img/icon-size-large.png" alt="" />
+                    </div>
+                    <div className="col-span-4 pl-4">
+                      <span className="block text-sm text-gray-600">Diện Tích:</span>
+                      <span className="block text-xs font-extrabold">700m2</span>
+                    </div>
+                  </div>
+                </div>
+              </div>
+              <div className="border-l border-gray-400 box-content">
+                <div className="pl-4">
+                  <div className="grid grid-cols-5">
+                    <div className="col-span-1">
+                      <img src="https://hanoioffice.vn/wp-content/plugins/mkdf-real-estate/assets/img/icon-van-phong-tron-goi-dark.png" alt="" />
+                    </div>
+                    <div className="col-span-4 pl-4">
+                      <span className="block text-sm text-gray-600">Phòng Làm Việc:</span>
+                      <span className="block text-xs font-extrabold">30 Phòng</span>
+                    </div>
+                  </div>
+                </div>
+              </div>
+              <div className="border-l border-gray-400 box-content">
+                <div className="pl-4">
+                  <div className="grid grid-cols-5">
+                    <div className="col-span-1">
+                      <img src="https://hanoioffice.vn/wp-content/plugins/mkdf-real-estate/assets/img/icon-cho-ngoi-lam-viec-dark.png" alt="" />
+                    </div>
+                    <div className="col-span-4 pl-4">
+                      <span className="block text-sm text-gray-600">Chỗ Ngồi:</span>
+                      <span className="block text-xs font-extrabold">Linh Hoạt + Cố Định</span>
+                    </div>
+                  </div>
+                </div>
+              </div>
+              <div className="border-l border-gray-400 box-content">
+                <div className="pl-4">
+                  <div className="grid grid-cols-5">
+                    <div className="col-span-1">
+                      <img src="https://hanoioffice.vn/wp-content/plugins/mkdf-real-estate/assets/img/icon-van-phong-ao-dark.png" alt="" />
+                    </div>
+                    <div className="col-span-4 pl-4">
+                      <span className="block text-sm text-gray-600">Văn phòng ảo:</span>
+                      <span className="block text-xs font-extrabold">1 Sàn</span>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div className="col-span-1">
+            <NavLink to="">
+              <span className="bg-blue-400 py-2 px-12 rounded">Đặt Lịch Tham Quan</span>
+            </NavLink>
+          </div>
+        </div>
+      </div>
     </>
   );
 };
